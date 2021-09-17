1458cd7429c0a9f5683fca2e1438a900f930e2a5
Tim Bowerbank <tim@roythezebra.com>
2021-09-17 12:59:55 +0100

    added new way of doing nav with uls and lis

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,7 +2,10 @@
 window.addEventListener("load", () => {
 
     // initNavScrollListener()
-    initNavScrollListener5();
+    //initNavScrollListener5();
+
+    // initPdDropdowns()
+    initPdDropdowns();
 
 });
 
@@ -51,4 +54,57 @@ function navScrollListener5() {
 
 // ************************
 // *** end pd-mainNav-5 ***
+// ************************
+
+// ********************
+// *** pd-mainNav-6 ***
+// ********************
+
+var pdDropdowns;
+var dropDownActive;
+function initPdDropdowns() {
+    // find all elements with the class .pd-dropdown
+    pdDropdowns = document.querySelectorAll(".pd-dropdown-toggle");
+   // loop through all dropdowns and add an event listener
+   for(const dropdown of pdDropdowns) {
+        dropdown.addEventListener("click", dropdownClickListener);
+   }
+}
+
+function dropdownClickListener(e) {
+    // prevent default behaviour
+    e.preventDefault();
+
+    // get the target and its next sibling which is the dropdown menu
+    let dropDownBtn = e.target;
+    dropDownActive = dropDownBtn.nextElementSibling;
+    dropDownActive.classList.toggle("show");
+    
+    // listen for click events on the document
+    document.addEventListener("click", closeDropDownListener);
+}
+
+function closeDropDownListener(e) {
+
+    if (!e.target.matches('.pd-dropdown-toggle')) {
+        // loop through all pdDropdowns and if they have show
+        dropDownActive.classList.toggle("show");
+
+        // loop through all .pd-dropdown-menu
+        let dropDownsToCheck = document.querySelectorAll(".pd-dropdown-menu");
+        for (const dropdown of dropDownsToCheck) {
+            if(dropdown.classList.contains("show")) {
+                dropdown.classList.toggle("show");
+            }
+        }
+
+    }
+
+
+
+}
+
+
+// ************************
+// *** end pd-mainNav-6 ***
 // ************************
