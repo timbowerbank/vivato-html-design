c3d258c86d2b11d878006964c2fbd65cc7c85098
Tim Bowerbank <tim@roythezebra.com>
2021-11-17 13:37:44 +0000

    Added steps carousel

--- assets/js/functions-sliders.js
+++ assets/js/functions-sliders.js
@@ -4,14 +4,27 @@ window.addEventListener("load", () => {
 
 });
 
+const COMPONENT_NAME = "data-glide";
+const COMPONENT_SELECTOR = `[${COMPONENT_NAME}]`;
+
 function initGlideSliders() {
     
-    const COMPONENT_NAME = "data-glide";
-    const COMPONENT_SELECTOR = `[${COMPONENT_NAME}]`;
+    // const COMPONENT_NAME = "data-glide";
+    // const COMPONENT_SELECTOR = `[${COMPONENT_NAME}]`;
   
     const components = document.querySelectorAll(COMPONENT_SELECTOR);
     
     for (let i = 0; i < components.length; i++) {
+
+        // check for any classes where these instances need special events
+        // using this for the step by step carousel
+        // if(components[i].classList.contains("pd-glide-events")) {
+        //     // invoke special init for this instance
+        //     initStepsGlideSlider(components[i]);
+        //     continue;
+        // }
+
+        // continue initialising for non event type sliders
         const options = JSON.parse(
             components[i].getAttribute(COMPONENT_NAME) || "{}"
         );
@@ -24,7 +37,44 @@ function initGlideSliders() {
         glide.mount();
     }
     
-
-
 }
+
+
+// *** initStepsGlideSliders() ***
+// function initStepsGlideSlider(nSliderInstance) {
+
+//     let glideInstanceId = nSliderInstance.id;
+    
+//     const options = JSON.parse(
+//         nSliderInstance.getAttribute(COMPONENT_NAME || "{}")
+//     );
+
+//     let glide = new Glide(nSliderInstance, options);
+
+//     // add events
+//     glide.on('run.after', (e) => {
+        
+//         const currentSlider = document.getElementById(glideInstanceId);
+//         const currentUl = currentSlider.getElementsByClassName("glide__slides")[0];
+//         const allSlides = currentUl.children;
+//         const currentSlideIndex = glide.index;
+        
+//         // console.log("glide index is: " + currentSlideIndex);
+//         for(let i = 0; i < allSlides.length; i++) {
+//             if(i === currentSlideIndex) {
+//                 // add pdActive to the slide
+//                 allSlides[i].classList.add("pdActive");
+//                 // console.log("just added pdActive to slide: " + i);
+//             } else {
+//                 if(allSlides[i].classList.contains("pdActive")) {
+//                     allSlides[i].classList.remove("pdActive");
+//                 }
+//             }
+//         }
+
+//     });
+
+//     glide.mount();
+
+// }
 
