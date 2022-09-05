a3bf16332ed4f0ff489dbe606b24d931ef64de8a
Tim Bowerbank <tim@roythezebra.com>
2022-09-05 13:06:43 +0100

    Added FAQs section

---
+++ assets/js/functions-faqs.js
@@ -0,0 +1,104 @@
+window.addEventListener("load", () => {
+    if(document.getElementsByClassName('pd-faqs-2') !== null) {
+        initPdFaqs2();
+    }
+
+
+
+});
+
+
+// initialise the Faq links
+function initPdFaqs2() {
+    
+    // get all instances of the pd-faqs-2
+    const allFaqs = document.getElementsByClassName("pd-faqs-2");
+    // loop through each instance
+    for(const faqBlock of allFaqs) {
+
+        // *** add listeners to btns
+        const allBtns = faqBlock.getElementsByClassName("pd-faqs-2-btn");
+        // loop through all buttons to add toggle listener
+        for(const btn of allBtns) {
+            btn.addEventListener("click", toggleFaqs2Listener);
+        }
+
+        // *** add transition end listeners
+        const faqBodies = faqBlock.getElementsByClassName("pd-faqs-2-card-body");
+        // loop through all bodies and add transition end
+        for(const faqBody of faqBodies) {
+            faqBody.addEventListener("transitionend", transitionEndFaq2Listener);
+        }
+    }
+
+
+}
+
+// *** toggleFaqs2Listener() ***
+function toggleFaqs2Listener(e) {
+    e.preventDefault();
+    
+    // assign the button to a variable
+    const faqBtn = e.target;
+
+    // check whether button or link
+    let faqOuter;
+    if(faqBtn.classList.contains("pd-faqs-2-open-close-outer")) {
+        faqOuter = faqBtn.parentElement.parentElement.parentElement;
+    } else {
+        faqOuter = faqBtn.parentElement.parentElement;
+    }
+
+    // identify the state of the dropdown - two levels up to parent
+    
+
+    // check state
+    if(faqOuter.classList.contains("pd-show-content")) {
+        // element is OPEN and needs to CLOSE
+
+        // remove the style and add pd-collapsing
+        const faqBody = faqOuter.querySelector(".pd-faqs-2-card-body");
+        faqBody.removeAttribute("style");
+        faqBody.classList.add("pd-collapsing");
+
+
+
+
+    } else {
+        // element is CLOSED and needs to OPEN
+
+        // add pd-show-content to outer
+        faqOuter.classList.add("pd-show-content");
+
+        // get a ref to the faqBody and its height
+        const faqBody = faqOuter.querySelector(".pd-faqs-2-card-body");
+        const faqBodyHeight = `height: ${faqBody.offsetHeight}px`;
+
+        // add pd-collapsing and force a repaint
+        faqBody.classList.add("pd-collapsing");
+        void(faqBody.offsetHeight);
+
+        // set the height on the style attribute
+        faqBody.setAttribute("style", faqBodyHeight);
+
+
+    }
+
+}
+
+
+// *** transitionEndFaq2Listener() ***
+function transitionEndFaq2Listener(e) {
+
+    const faqBody = e.target;
+    const faqOuter = faqBody.parentElement;
+
+    // remove pd-collapsing
+    faqBody.classList.remove("pd-collapsing");
+
+    // remove pd-show-content if style has a height
+    if(faqBody.getAttribute("style")== null || faqBody.getAttribute("style") == "") {
+        faqOuter.classList.remove("pd-show-content");
+    }
+
+}
